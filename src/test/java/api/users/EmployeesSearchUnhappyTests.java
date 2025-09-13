06e225ea88094cce27dadaf75a1157a5df8c4549
Andela-M <195332055+Andela-M@users.noreply.github.com>
2025-09-13 16:49:18 +0200

    test: add EmployeesSearchUnhappyTests class with tests

---
+++ src/test/java/api/users/EmployeesSearchUnhappyTests.java
@@ -0,0 +1,86 @@
+package api.users;
+
+import com.github.javafaker.Faker;
+import com.smartgarage.api.CustomerApi;
+import com.smartgarage.api.models.Users;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class EmployeesSearchUnhappyTests {
+    private final CustomerApi customerApi = new CustomerApi();
+    private final Faker faker = new Faker();
+    private String userName;
+    private String email;
+    private String phone;
+
+    @Test
+    @DisplayName("Admin should not find a non-existent customer by username")
+    void userNotFound_when_adminSearchesByNonExistentUsername()  {
+        userName = faker.name().username();
+        Users fetched = customerApi.findUserByUsername(userName);
+        assertNull(fetched, "User found with non-existing username.");
+    }
+
+    @Test
+    @DisplayName("Admin should not find a non-existent customer by email ")
+    void userNotFound_when_adminSearchesByNonExistentEmail() {
+        email = faker.internet().emailAddress();
+        Users fetched = customerApi.findUserByEmail(email);
+        assertNull(fetched, "User found with non-existing email.");
+    }
+
+    @Test
+    @DisplayName("Admin should not find a non-existent customer by phone")
+    void  userNotFound_when_adminSearchesByNonExistentPhone() {
+        phone = faker.number().digits(10);
+        Users fetched = customerApi.findUserByPhone(phone);
+        assertNull(fetched, "User found with non-existent phone.");
+
+    }
+
+    @ParameterizedTest(name = "Search Query: '{0}'")
+    @ValueSource(strings = {"", " "})
+    @DisplayName("API should not return all users on an empty search query")
+    void allUsers_notReturned_onEmptyQuery(String emptyQuery) {
+        Users[] results = customerApi.request()
+                .queryParam("username", emptyQuery)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(Users[].class);
+
+        assertEquals(0, results.length, "API returned all users for an empty search query.");
+    }
+
+    @ParameterizedTest(name = "Username: {0}")
+    @ValueSource(strings = {"a", "abcdefghijklmnopqrstuv"})
+    @DisplayName("API should reject invalid username length with a 400 Bad Request")
+    void apiRejects_invalidUsernameLength_with400(String invalidUsername) {
+        customerApi.request()
+                .queryParam("username", invalidUsername)
+                .when()
+                .get()
+                .then()
+                .statusCode(400);
+    }
+
+    @ParameterizedTest(name = "Email: {0}")
+    @ValueSource(strings = {"@bdoishd.com", "ehsgtd jdhys.com", "shgdtf@", "hagstdh@hf jdh.com"})
+    @DisplayName("API should reject invalid email format with a 400 Bad Request")
+    void apiRejects_invalidEmailFormat_with400(String invalidEmail) {
+        customerApi.request()
+                .queryParam("email", invalidEmail)
+                .when()
+                .get()
+                .then()
+                .statusCode(400);
+    }
+}
+
