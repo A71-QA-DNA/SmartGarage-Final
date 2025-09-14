293ac1b73ac7d24bf88bc90b20835018ffa3352c
Nikola Dimitrov <nikola.dimitrov.a71@learn.telerikacademy.com>
2025-09-14 04:54:26 +0300

    BaseWebPage amended. MainPage and LoginPage, created.

---
+++ src/main/java/com/smartgarage/pages/LoginPage.java
@@ -0,0 +1,67 @@
+package com.smartgarage.pages;
+
+import com.testframework.PropertiesManager;
+import com.testframework.core.BaseWebPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BaseWebPage {
+
+    @FindBy(id = "login-tab")
+    private WebElement loginTab;
+
+    @FindBy(id = "login-username")
+    private WebElement usernameField;
+
+    @FindBy(id = "login-password")
+    private WebElement passwordField;
+
+    @FindBy(xpath = "//div[@id='login-form']//button")
+    private WebElement loginButton;
+
+    @FindBy(id = "register-tab")
+    private WebElement registerTab;
+
+    @FindBy(id = "reg-username")
+    private WebElement regUsernameField;
+
+    @FindBy(id = "reg-email")
+    private WebElement regEmailField;
+
+    @FindBy(id = "reg-first-name")
+    private WebElement regFirstNameField;
+
+    @FindBy(id = "reg-last-name")
+    private WebElement regLastNameField;
+
+    @FindBy(id = "reg-phone")
+    private WebElement regPhoneField;
+
+    @FindBy(xpath = "//div[@id='register-form']//button")
+    private WebElement registerButton;
+
+    protected LoginPage(WebDriver webDriver) {
+        super(webDriver, "auth/login");
+        PageFactory.initElements(webDriver, this);
+    }
+
+    public LoginPage enterUsername(String username) {
+        waitForElementToBeVisible(usernameField);
+        usernameField.clear();
+        usernameField.sendKeys(username);
+        return this; // Enable method chaining
+    }
+
+    public LoginPage enterPassword(String password) {
+        passwordField.clear();
+        passwordField.sendKeys(password);
+        return this;
+    }
+
+    @Override
+    protected String getPageUrl() {
+        return super.getPageUrl();
+    }
+}
--- src/main/java/com/testframework/core/BaseWebPage.java
+++ src/main/java/com/testframework/core/BaseWebPage.java
@@ -1,40 +1,79 @@
 package com.testframework.core;
 
-import com.testframework.Driver;
-import com.testframework.DriverManager;
-import org.junit.jupiter.api.Assertions;
+import com.testframework.PropertiesManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 
 public abstract class BaseWebPage {
+
+    protected final WebDriver webDriver;
     private final String pageUrl;
+    protected final WebDriverWait webDriverWait;
 
-    // Url
-
-    public abstract String getBasePageUrl();
-
-    public BaseWebPage(String pageSpecificUrl) {
-        pageUrl = pageSpecificUrl;
+    protected BaseWebPage(WebDriver webDriver, String pageUrl) {
+        this.webDriver = webDriver;
+        this.pageUrl = pageUrl;
+        this.webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(Integer.parseInt(PropertiesManager.getConfigProperties().getProperty("defaultTimeoutSeconds"))));
+        PageFactory.initElements(webDriver, this);
     }
 
-    protected Driver driver() {
-        return DriverManager.getDriver();
+    protected BaseWebPage(WebDriver webDriver) {
+        this(webDriver, "");
     }
 
-    protected WebDriverWait driverWait() {
-        return driver().getDriverWait();
+    protected void waitForElementToBeVisible(WebElement webElement) {
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
     }
 
-    public String getPageUrl() {
-        return getBasePageUrl() + pageUrl;
+    protected void waitForElementToBeClickable(WebElement webElement) {
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(webElement));
     }
 
-    // Actions
-    public void navigate() {
-        driver().get(getPageUrl());
+    @FindBy(xpath = "//a[@title='CarService']")
+    protected WebElement mainHeading;
+
+    protected String getPageUrl() {
+        return PropertiesManager.getConfigProperties().getProperty("smartGarageUrl") + pageUrl;
     }
 
-    public void assertNavigated() {
-        Assertions.assertEquals(getPageUrl(), driver().getCurrentUrl(), "Page was not navigated.");
+    public void open() {
+        webDriver.navigate().to(getPageUrl());
+        waitForElementToBeVisible(mainHeading);
     }
+//    private final String pageUrl;
+//
+//    // Url
+//
+//    public abstract String getBasePageUrl();
+//
+//    public BaseWebPage(String pageSpecificUrl) {
+//        pageUrl = pageSpecificUrl;
+//    }
+//
+//    protected Driver driver() {
+//        return DriverManager.getDriver();
+//    }
+//
+//    protected WebDriverWait driverWait() {
+//        return driver().getDriverWait();
+//    }
+//
+//    public String getPageUrl() {
+//        return getBasePageUrl() + pageUrl;
+//    }
+//
+//    // Actions
+//    public void navigate() {
+//        driver().get(getPageUrl());
+//    }
+//
+//    public void assertNavigated() {
+//        Assertions.assertEquals(getPageUrl(), driver().getCurrentUrl(), "Page was not navigated.");
+//    }
 }
