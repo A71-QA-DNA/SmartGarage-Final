b38b5f580723c90231e16af111d6e8576fe97fe0
Andela-M <195332055+Andela-M@users.noreply.github.com>
2025-09-14 22:54:02 +0200

    pages: implement CreateCustomerPage using PageFactory

--- src/main/java/com/smartgarage/pages/AdminPanelPage.java
+++ src/main/java/com/smartgarage/pages/AdminPanelPage.java
@@ -5,23 +5,17 @@ import org.openqa.selenium.support.FindBy;
 
 public class AdminPanelPage extends BaseSmartGaragePage {
 
-    @FindBy(xpath = "//a[@href='/users']")
-    private WebElement allUsersTab;
+    @FindBy(xpath = "//a[@href='/users']") private WebElement allUsersTab;
 
-    @FindBy(xpath = "//a[@href='/client-cars']")
-    private WebElement clientCars;
+    @FindBy(xpath = "//a[@href='/client-cars']") private WebElement clientCars;
 
-    @FindBy(xpath = "//a[@href='/orders']")
-    private WebElement orders;
+    @FindBy(xpath = "//a[@href='/orders']") private WebElement orders;
 
-    @FindBy(xpath = "//a[@href='/users/employee']")
-    private WebElement createEmployee;
+    @FindBy(xpath = "//a[@href='/users/employee']") private WebElement createEmployee;
 
-    @FindBy(xpath = "//a[@href='/users/mechanic']")
-    private WebElement createMechanic;
+    @FindBy(xpath = "//a[@href='/users/mechanic']") private WebElement createMechanic;
 
-    @FindBy(xpath = "//a[@href='/users/customer']")
-    private WebElement createCustomer;
+    @FindBy(xpath = "//a[@href='/users/customer']") private WebElement createCustomer;
 
     public AdminPanelPage() {super("admin");}
 
