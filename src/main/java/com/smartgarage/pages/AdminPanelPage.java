462f5c6b05ec74dcf4655b7c1aefaef1cd7abd24
Andela-M <195332055+Andela-M@users.noreply.github.com>
2025-09-14 22:13:16 +0200

    pages: implement AdminPanelPage using PageFactory

--- src/main/java/com/smartgarage/pages/AdminPanelPage.java
+++ src/main/java/com/smartgarage/pages/AdminPanelPage.java
@@ -1,8 +1,57 @@
 package com.smartgarage.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AdminPanelPage extends BaseSmartGaragePage {
 
-    public AdminPanelPage() {
-       super("admin");
+    @FindBy(xpath = "//a[@href='/users']")
+    private WebElement allUsersTab;
+
+    @FindBy(xpath = "//a[@href='/client-cars']")
+    private WebElement clientCars;
+
+    @FindBy(xpath = "//a[@href='/orders']")
+    private WebElement orders;
+
+    @FindBy(xpath = "//a[@href='/users/employee']")
+    private WebElement createEmployee;
+
+    @FindBy(xpath = "//a[@href='/users/mechanic']")
+    private WebElement createMechanic;
+
+    @FindBy(xpath = "//a[@href='/users/customer']")
+    private WebElement createCustomer;
+
+    public AdminPanelPage() {super("admin");}
+
+    public void clickAllUsers() {
+        waitForElementToBeClickable(allUsersTab);
+        allUsersTab.click();
+    }
+
+    public void clickClientCars() {
+        waitForElementToBeClickable(clientCars);
+        clientCars.click();
+    }
+
+    public void clickOrders() {
+        waitForElementToBeClickable(orders);
+        orders.click();
+    }
+
+    public void clickCreateEmployee() {
+        waitForElementToBeClickable(createEmployee);
+        createEmployee.click();
+    }
+
+    public void clickCreateMechanic() {
+        waitForElementToBeClickable(createMechanic);
+        createMechanic.click();
+    }
+
+    public void clickCreateCustomer() {
+        waitForElementToBeClickable(createCustomer);
+        createCustomer.click();
     }
 }
