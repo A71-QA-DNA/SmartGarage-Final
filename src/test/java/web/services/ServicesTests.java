05072ac95d561f509651b1840227a279eaa4d1c1
danailzahariev <d.zahariev@icloud.com>
2025-09-15 16:21:28 +0300

    Added ServicesTests with Selenium

--- src/test/java/web/services/ServicesTests.java
+++ src/test/java/web/services/ServicesTests.java
@@ -2,8 +2,7 @@ package web.services;
 
 import com.testframework.core.BaseWebTest;
 import io.qameta.allure.Story;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,12 +10,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ServicesTests extends BaseWebTest {
 
-    private static final int TEST_SERVICE_ID = 25;
+    private static final String TEST_SERVICE_ID = "25";
+    private static final String ADMIN_USERNAME = "felix_jackson";
+    private static final String ADMIN_PASSWORD = "password123%D";
 
     @BeforeEach
-    public void beforeEach() {
+    public void beforeAll() {
         loginPage.navigate();
-        loginPage.loginAs("felix_jackson", "password123%D");
+        loginPage.loginAs(ADMIN_USERNAME, ADMIN_PASSWORD);
     }
 
     @AfterEach
@@ -26,7 +27,7 @@ public class ServicesTests extends BaseWebTest {
 
 
     @ParameterizedTest(name = "Edit service price → new value={0}, expectedSuccess={1}")
-    @ValueSource(strings = {"00.00", "01.00", "-1",""})
+    @ValueSource(strings = {"00.00", "01.00", "-1"})
     @Story("Each service must have a price. The price must be a non-negative number.")
     public void whenEditingServicePrice_validateNonNegativeInput(String newPrice) {
         servicesPage.navigate();
@@ -42,5 +43,36 @@ public class ServicesTests extends BaseWebTest {
             String actualPrice = serviceOverviewPage.getServicePrice(TEST_SERVICE_ID);
             assertEquals(newPrice, actualPrice);
         }
+    }
+
+    @ParameterizedTest(name = "Create service with price={0} → expectSaved={1}")
+    @ValueSource(strings = {"00.00", "01.00", "-1"})
+    void addService_validatePrice_andCleanup(String price) {
+        servicesPage.navigate();
+        servicesPage.clickBrakeRepairLink();
+
+        int before = serviceOverviewPage.getServiceCount();
+        String name = "UI Test Service " + System.currentTimeMillis();
+
+        serviceOverviewPage.clickAddService();
+        serviceOverviewPage.enterNewService(name, price);
+        serviceOverviewPage.clickSaveNewService();
+
+        if (price.equals("-1")) {
+            driver().navigate().refresh();
+            assertEquals(before, serviceOverviewPage.getServiceCount());
+        } else {
+            String newId = serviceOverviewPage.findNewServiceId(name);
+
+            int actual = serviceOverviewPage.getServiceCount();
+            String expectedName = serviceOverviewPage.getServiceRowByName(newId);
+            String expectedPrice = serviceOverviewPage.getServicePrice(newId);
+
+            assertEquals(expectedName, name);
+            assertEquals(expectedPrice, price);
+            assertNotEquals(before, actual);
+            serviceOverviewPage.deleteServiceById(newId);
+        }
+
     }
 }
