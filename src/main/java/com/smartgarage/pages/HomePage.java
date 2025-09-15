4fcfccaf299bfab5dcb2b8f3a6d539227161a50b
Andela-M <195332055+Andela-M@users.noreply.github.com>
2025-09-15 23:02:56 +0200

    pages: Add getHeaderText function in HomePage

--- src/main/java/com/smartgarage/pages/HomePage.java
+++ src/main/java/com/smartgarage/pages/HomePage.java
@@ -1,8 +1,18 @@
 package com.smartgarage.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage extends BaseSmartGaragePage {
+
+    @FindBy(xpath = "//a[@title='CarService']") private WebElement smartGarageHeader;
 
     public HomePage() {
         super("");
+    }
+
+    public String getHeaderText(){
+        waitForElementToBeVisible(smartGarageHeader);
+        return smartGarageHeader.getText();
     }
 }
