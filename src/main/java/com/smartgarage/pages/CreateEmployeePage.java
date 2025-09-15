7c4058a15fbb4994119cf4019d53e0537908b8fc
Andela-M <195332055+Andela-M@users.noreply.github.com>
2025-09-15 08:49:29 +0200

    pages: implement CreateEmployeePage using PageFactory

---
+++ src/main/java/com/smartgarage/pages/CreateEmployeePage.java
@@ -0,0 +1,35 @@
+package com.smartgarage.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CreateEmployeePage extends BaseSmartGaragePage{
+    @FindBy(id = "reg-username") private WebElement usernameField;
+    @FindBy(id = "reg-email") private WebElement emailField;
+    @FindBy(id = "reg-first-name") private WebElement firstNameField;
+    @FindBy(id = "reg-last-name") private WebElement lastNameField;
+    @FindBy(id = "reg-phone") private WebElement phoneField;
+    @FindBy(xpath = "//button[@type='submit']") private WebElement registerButton;
+
+    public CreateEmployeePage(){
+        super("users/employee");
+    }
+
+    public void fillForm(String username, String email, String firstName, String lastName, String phone){
+        usernameField.clear();
+        usernameField.sendKeys(username);
+        emailField.clear();
+        emailField.sendKeys(email);
+        firstNameField.clear();
+        firstNameField.sendKeys(firstName);
+        lastNameField.clear();
+        lastNameField.sendKeys(lastName);
+        phoneField.clear();
+        phoneField.sendKeys(phone);
+    }
+
+    public void clickRegisterButton(){
+        registerButton.click();
+    }
+}
+
