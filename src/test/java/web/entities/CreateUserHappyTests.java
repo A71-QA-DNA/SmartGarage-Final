6493b1bcc0b53dbba24d9037e003e489199570fe
Andela-M <195332055+Andela-M@users.noreply.github.com>
2025-09-19 18:17:20 +0200

    test web: refactor CreateUserHappyTests with new FakerHelper

--- src/test/java/web/entities/CreateUserHappyTests.java
+++ src/test/java/web/entities/CreateUserHappyTests.java
@@ -3,39 +3,36 @@ package web.entities;
 import com.smartgarage.api.CustomerApi;
 import com.smartgarage.api.models.Users;
 import com.testframework.core.BaseWebTest;
+import helper.FakerHelper;
+import helper.UserData;
 import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Epic("Entities")
+@Feature("User Registration by Admin")
 @Tag("system")
 public class CreateUserHappyTests extends BaseWebTest {
 
-    private CustomerApi  customerApi;
-    private String userName;
-    private String email;
-    private String firstName;
-    private String lastName;
-    private String phone;
+    private CustomerApi customerApi;
+    private UserData userData;
     private Integer userId;
+
 
     @BeforeEach
     public void setup() {
         loginPage.navigate();
         loginPage.loginAs("felix_jackson", "password123%D");
-        userName = faker.name().username();
-        email = faker.internet().emailAddress();
-        firstName = faker.name().firstName();
-        lastName = faker.name().lastName();
-        phone = faker.number().digits(10);
+        userData = FakerHelper.createRandomUser();
     }
 
     @AfterEach
     public void tearDown() {
         customerApi = new CustomerApi();
-        Users fetched = customerApi.findUserByUsername(userName);
+        Users fetched = customerApi.findUserByUsername(userData.getUserName());
         assertNotNull(fetched, "User not found after creation.");
         userId = fetched.getUserId();
         assertNotNull(userId, "User ID was not returned by search.");
@@ -47,9 +44,15 @@ public class CreateUserHappyTests extends BaseWebTest {
     void customerSuccessfullyCreated() {
         navigationSection.clickAdminPanelButton();
         adminPanelPage.clickCreateCustomer();
-        createCustomerPage.fillForm(userName, email, firstName, lastName, phone);
+        createCustomerPage.fillForm(
+                userData.getUserName(),
+                userData.getEmail(),
+                userData.getFirstName(),
+                userData.getLastName(),
+                userData.getPhone()
+                );
         createCustomerPage.clickRegisterButton();
-        assertEquals(userName, createCustomerPage.getCreatedUsername(), "Customer was not created.");
+        assertEquals(userData.getUserName(), createCustomerPage.getCreatedUsername(), "Customer was not created.");
     }
 
     @Test
@@ -57,9 +60,14 @@ public class CreateUserHappyTests extends BaseWebTest {
     void employeeSuccessfullyCreated() {
         navigationSection.clickAdminPanelButton();
         adminPanelPage.clickCreateEmployee();
-        createEmployeePage.fillForm(userName, email, firstName, lastName, phone);
+        createEmployeePage.fillForm(userData.getUserName(),
+                userData.getEmail(),
+                userData.getFirstName(),
+                userData.getLastName(),
+                userData.getPhone()
+        );
         createEmployeePage.clickRegisterButton();
-        assertEquals(userName, createEmployeePage.getCreatedUsername(), "Employee was not created.");
+        assertEquals(userData.getUserName(), createEmployeePage.getCreatedUsername(), "Employee was not created.");
     }
 
     @Test
@@ -67,9 +75,13 @@ public class CreateUserHappyTests extends BaseWebTest {
     void  mechanicSuccessfullyCreated() {
         navigationSection.clickAdminPanelButton();
         adminPanelPage.clickCreateMechanic();
-        createMechanicPage.fillForm(userName, email, firstName, lastName, phone);
+        createMechanicPage.fillForm(userData.getUserName(),
+                userData.getEmail(),
+                userData.getFirstName(),
+                userData.getLastName(),
+                userData.getPhone()
+        );
         createMechanicPage.clickRegisterButton();
-        assertEquals(userName, createMechanicPage.getCreatedUsername(), "Employee was not created.");
-        System.out.println(userName);
+        assertEquals(userData.getUserName(), createMechanicPage.getCreatedUsername(), "Employee was not created.");
     }
 }
